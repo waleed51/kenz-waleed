d20204a22700678c72d7bb744771a4e3ac6d741a
waleed <waleed.abdelhameed@cloud-shelf.com>
2024-10-02 21:14:16 +0300

    - add integration tests - update needed dependancies / packages - update Program file for test purposes

--- KanzWayScreening.API/Program.cs
+++ KanzWayScreening.API/Program.cs
@@ -1,33 +1,39 @@
 using KanzWayScreening.Application.Common.DI;
 using KanzWayScreening.Application.Screening.Queries;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.RegisterApplicationServices();
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetScreeningResultQueryHandler>());
-
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
+    public static void Main(string[] args)
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "KanzWay API v1");
-    });
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.RegisterApplicationServices();
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetScreeningResultQueryHandler>());
+
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "KanzWay API v1");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
